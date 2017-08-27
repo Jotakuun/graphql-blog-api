c0577789e71fb77ef7bbbbc33cd3982a594c72f1
Juan M. Rivas <jotakuun@gmail.com>
2017-08-27 20:00:33 +0200

    login query and authentication middleware :zap:

--- src/graphql/queries/user-queries.js
+++ src/graphql/queries/user-queries.js
@@ -1,11 +1,12 @@
 import { GraphQLString } from 'graphql';
 import {Types} from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
-import { userType } from '../types/user';
 import UserModel from '../../models/user-model';
 
 let login = {
-    type: userType,
+    type: GraphQLString,
     args: {
         username: { 
             name: 'username',
@@ -16,8 +17,30 @@ let login = {
             type: GraphQLString
         }
     },
-    resolve (root, params, context, info) {
-    }
+    async resolve (root, {username, password}, {secret}) {
+        const user = await UserModel.findOne({ username: username }, '_id username password admin');
+        if (!user) {
+          throw new Error(`There isn't any user matching that username`);
+        }
 
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+          throw new Error('Incorrect password');
+        }
+
+        const token = jwt.sign(
+          {
+            user: {
+                id: user._id,
+                username: user.username,
+                isAdmin: user.admin
+            }
+          },
+          secret,
+          { expiresIn: '1h' },
+        );
+  
+        return token;
+    }
 }
 export default { login };
--- src/server.js
+++ src/server.js
@@ -3,19 +3,42 @@ import graphqlHTTP from 'express-graphql';
 import  {buildSchema}  from 'graphql';
 import config from 'config';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 
 import schema from './graphql';
 import BlogPostModel from './models/blog-post-model';
 
-let DBHost = config.get('DBHost')
+const API_SECRET = config.get('API_SECRET');
+const DBHost = config.get('DBHost')
 mongoose.connect(DBHost, { useMongoClient: true });
 
-let app = express();
-app.use('/graphql', graphqlHTTP({
+const app = express();
+
+const getUser = async (req) => {
+  const token = req.headers.authorization;
+  try {
+    if(token) {
+      let decoded = await jwt.verify(token, API_SECRET);
+      req.user = decoded.user;
+    }
+  } catch (err) {
+    console.log('user not authenticated');
+  }
+  req.next();
+};
+
+app.use(getUser);
+
+app.use('/graphql', graphqlHTTP((req) => ({
   schema: schema,
   pretty: true,
   graphiql: config.util.getEnv('NODE_ENV') !== 'production',
-}))
+  context: {
+    secret: API_SECRET,
+    user: req.user
+  }
+})
+));
 
 app.listen(4000);
 console.log('Running server at localhost:4000')
